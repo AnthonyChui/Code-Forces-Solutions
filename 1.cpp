2939111860e43909052a1d746b5732203f807f06
Anthony Chui <154588467+AnthonyChui@users.noreply.github.com>
2024-07-30 19:49:54 -0600

    Add 1A solution

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int getNumStones(int n, int m, int a){
+    long long numStones;
+    int x = 0;
+    int y = 0;
+    x = m/a;
+    if(m%a!=0){
+    x++;
+    }
+    y = n/a;
+    if(n%a!=0){
+    y++;
+    }
+    numStones = x*y;
+    return numStones;
+}
+
+int main(){
+    int n,m,a;
+    cin>>n>>m>>a;
+    cout<<getNumStones(n,m,a)<<endl;
+    return 0;
+}
